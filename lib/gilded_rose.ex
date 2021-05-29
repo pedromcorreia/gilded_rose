188bdd530cbe2fa172dd03e79023a321d5806a52
pedromcorreia <pedmcor@gmail.com>
2021-05-29 18:55:21 -0300

    Remove old function for update_quality/1

--- lib/gilded_rose.ex
+++ lib/gilded_rose.ex
@@ -27,6 +27,17 @@ defmodule GildedRose do
 
   def items(agent), do: Agent.get(agent, & &1)
 
+  def update_quality(agent) do
+    for i <- 0..(Agent.get(agent, &length/1) - 1) do
+      item = Agent.get(agent, &Enum.at(&1, i))
+      item = update_item(item)
+
+      Agent.update(agent, &List.replace_at(&1, i, item))
+    end
+
+    :ok
+  end
+
   def update_item(%Item{name: @backstage, sell_in: sell_in} = item) when sell_in <= 0 do
     %{item | quality: 0} |> decrease_sell_in
   end
@@ -79,133 +90,5 @@ defmodule GildedRose do
 
   defp decrease_sell_in(%Item{sell_in: sell_in} = item) do
     %{item | sell_in: sell_in - 1}
-  end
-
-  def update_quality(agent) do
-    for i <- 0..(Agent.get(agent, &length/1) - 1) do
-      item = Agent.get(agent, &Enum.at(&1, i))
-      item = update_item(item)
-
-      Agent.update(agent, &List.replace_at(&1, i, item))
-    end
-
-    :ok
-  end
-
-  # keep while refactoring
-  def update_quality(agent, :leeroy) do
-    for i <- 0..(Agent.get(agent, &length/1) - 1) do
-      item = Agent.get(agent, &Enum.at(&1, i))
-
-      item =
-        cond do
-          item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" ->
-            if item.quality > 0 do
-              if item.name != "Sulfuras, Hand of Ragnaros" do
-                %{item | quality: item.quality - 1}
-              else
-                item
-              end
-            else
-              item
-            end
-
-          true ->
-            cond do
-              item.quality < 50 ->
-                item = %{item | quality: item.quality + 1}
-
-                cond do
-                  item.name == "Backstage passes to a TAFKAL80ETC concert" ->
-                    item =
-                      cond do
-                        item.sell_in < 11 ->
-                          cond do
-                            item.quality < 50 ->
-                              %{item | quality: item.quality + 1}
-
-                            true ->
-                              item
-                          end
-
-                        true ->
-                          item
-                      end
-
-                    cond do
-                      item.sell_in < 6 ->
-                        cond do
-                          item.quality < 50 ->
-                            %{item | quality: item.quality + 1}
-
-                          true ->
-                            item
-                        end
-
-                      true ->
-                        item
-                    end
-
-                  true ->
-                    item
-                end
-
-              true ->
-                item
-            end
-        end
-
-      item =
-        cond do
-          item.name != "Sulfuras, Hand of Ragnaros" ->
-            %{item | sell_in: item.sell_in - 1}
-
-          true ->
-            item
-        end
-
-      item =
-        cond do
-          item.sell_in < 0 ->
-            cond do
-              item.name != "Aged Brie" ->
-                cond do
-                  item.name != "Backstage passes to a TAFKAL80ETC concert" ->
-                    cond do
-                      item.quality > 0 ->
-                        cond do
-                          item.name != "Sulfuras, Hand of Ragnaros" ->
-                            %{item | quality: item.quality - 1}
-
-                          true ->
-                            item
-                        end
-
-                      true ->
-                        item
-                    end
-
-                  true ->
-                    %{item | quality: item.quality - item.quality}
-                end
-
-              true ->
-                cond do
-                  item.quality < 50 ->
-                    %{item | quality: item.quality + 1}
-
-                  true ->
-                    item
-                end
-            end
-
-          true ->
-            item
-        end
-
-      Agent.update(agent, &List.replace_at(&1, i, item))
-    end
-
-    :ok
   end
 end
